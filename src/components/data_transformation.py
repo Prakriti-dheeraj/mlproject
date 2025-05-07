f174d45bf0e86f0413cb82280a5bc2cd54699c16
Prakriti-dheeraj <prakritidheeraj1224@gmail.com>
2025-05-07 16:27:13 +0530

    model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -15,7 +15,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_obj_file_path = os.path.join('artifact', 'preprocessor.plk')
+    preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.plk')
 
 class DataTransformation:
     def __init__(self):
